98814aa8496d5d61e565832cdeb027a79b18187f
ADIL <anwaradil295@gmail.com>
2025-11-13 14:25:04 +0530

    github deployments

--- next.config.js
+++ next.config.js
@@ -2,7 +2,7 @@
 const isProd = process.env.NODE_ENV === 'production';
 
 const nextConfig = {
-  // output: 'export', // enables static export for GitHub Pages
+  output: 'export', // enables static export for GitHub Pages
   eslint: {
     ignoreDuringBuilds: true,
   },
